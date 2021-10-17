34d4790277c5f9014d8ab958df51eeddc0e187b1
Ruszka Tamas <ruszka.tamas@gmail.com>
2021-10-17 19:27:52 +0200

    Converting to two's complement

--- src/SIC45x.cpp
+++ src/SIC45x.cpp
@@ -4,7 +4,9 @@ double SIC45X_parseLinear11(uint16_t rawValue) {
 	uint16_t exponent = (rawValue >> 11) & 0b1111;
 	uint8_t  exponentSign = rawValue >> 15;
 
-	exponent = (exponentSign ? -1 : 1) * exponent;
+	if (exponentSign) {
+		exponent = -1 * (exponent ^ 0b1111) - 1;
+	}
 
 	return SIC45X_parseLinear11(exponent, rawValue);
 }
@@ -13,7 +15,9 @@ double SIC45X_parseLinear11(uint8_t exponent, uint16_t rawValue) {
 	uint16_t mantissa = rawValue & 1023;
 	uint8_t  mantissaSign = (rawValue >> 10) & 1;
 
-	mantissa = (mantissaSign ? -1 : 1) * mantissa;
+	if (mantissaSign) {
+		mantissa = -1 * (mantissa ^ 1023) - 1;
+	}
 
 	return pow(2, exponent) * mantissa;
 }
